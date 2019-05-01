f1677b47078b6cd49cf388e51ab01e6a24f8d0d5
Doran <dorankayoumi@gmail.com>
2019-05-01 14:08:53 +0200

    Add method def

--- ArrayDeque.h
+++ ArrayDeque.h
@@ -24,13 +24,40 @@ private:
     size_type taille;
 
 public:
-    ArrayDeque(size_type cap = 0) : debut(0), taille(0), capacite(cap)
-    {
+    ArrayDeque(size_type cap = 0) : debut(0), taille(0), capacite(cap) {
         buffer = capacite != 0 ?
                  (pointer) ::operator new(capacite * sizeof(value_type))
                                : nullptr;
     }
 
+
+    size_type size() const {
+        return taille;
+    }
+
+    size_type capacity() const {}
+
+    bool empty() const {}
+
+    value_type at() const {}
+
+    reference at() {}
+
+    value_type back() const {}
+
+    reference back() {}
+
+    value_type front() const {}
+
+    reference front() {}
+
+    void push_back(const_reference value) {}
+
+    void push_front(const_reference value) {}
+
+    void pop_back() {}
+
+    void pop_front() {}
 
     // Completer cette classe générique pour qu'elle passe le codecheck
 
